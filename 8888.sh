69e17cf67efc858b6a267cff231503d1658ff7dd
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2025-01-21 13:30:10 +0300

    Update 8888.sh

--- 8888.sh
+++ 8888.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 clear
-
+# Renkli yazdırma fonksiyonları
 printGreen() {
     echo -e "\033[32m$1\033[0m"
 }
 
-
+# ASCII sanatlarını yazdırma fonksiyonu
 printAsciiArt() {
     echo -e "\033[32m$1\033[0m"
 }
 
-
+# ASCII sanatları
 ascii1="
                                                                                                     
                                                                                                     
@@ -174,7 +174,7 @@ ascii3="█                                                   █
                                                                                                                   
 "
 
-
+# User confirmation
 echo -n "Are you ready to see Hazen's favorite Omies? If you are ready, type 'yes': "
 read user_input
 
@@ -183,7 +183,7 @@ if [[ "$user_input" != "yes" ]]; then
     exit 1
 fi
 
-
+# Display ASCII art sequentially
 printAsciiArt "$ascii1"
 sleep 2
 clear
@@ -192,18 +192,18 @@ sleep 2
 clear
 printAsciiArt "$ascii3"
 
-
+# Complete the operation
 printGreen "You can go and check Hazen's favorite here: https://www.stargaze.zone/m/onchain-omies/4732
 For the entire collection: https://www.stargaze.zone/m/onchain-omies/tokens"
 
-
+# Wait for 10 seconds
 sleep 10
 
-
+# Prompt to return to the main menu
 echo "Press any key to return to the main menu."
 read -n 1 -s
 
-
+# Return to the main menu
 clear
 bash <(curl -s https://raw.githubusercontent.com/hazennetworksolutions/omies/refs/heads/main/8.sh)
 continue
